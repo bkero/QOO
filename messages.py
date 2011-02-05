87893cb6ddff78ad821697d1cc83200e9258f848
Trevor Hardcastle <chizu@spicious.com>
2011-02-04 21:09:46 -0800

    Start of some messaging code.

--- messages.py
+++ messages.py
@@ -1,3 +1,18 @@
 import beanstalk
+from twisted.internet import protocol
 
-beanstalk
+def jobCall(bs, job_dict):
+    bs.use("qoo")
+    d = bs.put(job_dict)
+    if LOG > 0:
+        d.addCallback(
+            lambda x: log.msg("Queued job: %s\n" % `x`))
+
+def job(d, job, **kwargs):
+    d.addCallback(jobCall, {"job":job, "payload":kwargs})
+
+def connect(reactor):
+    cc = protocol.ClientCreator(reactor,
+                                beanstalk.twisted_client.Beanstalk)
+    d = cc.connectTCP("localhost", 11300)
+    return d
