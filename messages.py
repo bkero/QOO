e9780f48d4e016476c3942e5ce28e73a2e664fa1
Trevor Hardcastle <chizu@spicious.com>
2011-02-04 22:30:20 -0800

    Listen for jobs.

--- messages.py
+++ messages.py
@@ -1,14 +1,37 @@
 import beanstalk
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, protocol, task
 from twisted.python import log
 import json
 
+import objects
+
 LOG = 1
+
+def executor(v, bs):
+    bs.touch(jobdata['jid'])
+    if LOG:
+        log.msg(v)
+    bs.delete(jobdata['jid'])
+
+def executionGenerator(bs):
+    while True:
+        if LOG:
+            log.msg("Waiting for job...")
+        job = bs.reserve()
+        job.addCallback(executor, bs)
+        yield job
+
+def jobListen(bs, channel):
+    bs.watch(channel)
+    bs.ignore("default")
+    
+    coop = task.Cooperator()
+    coop.coiterate(executionGenerator(bs))
 
 def jobCall(bs, channel, job_dict):
     bs.use(channel)
     d = bs.put(json.dumps(job_dict))
-    if LOG > 0:
+    if LOG:
         d.addCallback(
             lambda x: log.msg("Queued job: %s\n" % `x`))
 
@@ -20,6 +43,9 @@ class JobServer(object):
 
     def add(self, job, channel = "system", **kwargs):
         self.deferred.addCallback(jobCall, channel, {"job":job, "payload":kwargs})
+
+    def listen(self, channel = "system"):
+        self.deferred.addCallback(jobListen, channel)
 
     def createObject(self, class_name, name):
         self.add("create", type=class_name, name=name)
